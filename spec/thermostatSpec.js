af43a1592a7b96ac796f101c3d953251dc6dad22
Robert Fishwick <fishwickrob@live.com>
2019-05-27 16:37:48 +0100

    Energy usage status

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,30 +2,39 @@ describe('Thermostat', function () {
   var Thermostat  = require('../lib/thermostat');
   var thermostat = new Thermostat();
 
+  afterEach(function () {
+    thermostat.reset();
+    });
+
 
 
   describe('Temperature Property of Thermostat', function () {
+    afterEach(function () {
+      thermostat.reset();
+      });
+
     it('should have a default temperature of 20 degrees', function () {
       expect(thermostat.temperature).toEqual(20);
     });
   });
 
   describe('Changing temperature', function () {
+    afterEach(function () {
+      thermostat.reset();
+      });
+
     it('should increase temperature by a specified amount', function () {
       thermostat.temperatureUp(5);
       expect(thermostat.temperature).toEqual(25);
-      thermostat.reset();
     });
     it('should decrease temperature by a specified amount', function () {
       thermostat.temperatureDown(5);
       expect(thermostat.temperature).toEqual(15);
-      thermostat.reset();
     });
     it('should prevent temperature from decreasing below 10', function () {
       expect(function () {
         thermostat.temperatureDown(11)
       }).toThrow('Minimum temperature is 10 degrees');
-      thermostat.reset();
     });
   });
 
@@ -33,7 +42,6 @@ describe('Thermostat', function () {
     expect(function () {
       thermostat.temperatureUp(6)
     }).toThrow('Maximum temperature is 25 degrees');
-    thermostat.reset();
   })
 
   it('should prevent temperature from rising above 32 when power saver off', function () {
@@ -41,10 +49,14 @@ describe('Thermostat', function () {
     expect(function () {
       thermostat.temperatureUp(13);
     }).toThrow('Maximum temperature is 32 degrees');
-    thermostat.reset();
   })
 
   describe('Power saving mode', function(){
+    afterEach(function () {
+      thermostat.reset();
+      });
+
+
     it('be set to on by default', function(){
       expect(thermostat.powerSaver).toBeTruthy();
     });
@@ -52,14 +64,36 @@ describe('Thermostat', function () {
     it('should switch off', function(){
       thermostat.powerSaverOff();
       expect(thermostat.powerSaver).toBeFalsy();
-      thermostat.reset();
     });
 
     it('should switch on', function(){
       thermostat.powerSaverOff();
       thermostat.powerSaverOn();
       expect(thermostat.powerSaver).toBeTruthy();
+    });
+  });
+
+  it('should reset the temperature to 20', function () {
+      thermostat.temperatureUp(2);
+      thermostat.resetTemperature();
+      expect(thermostat.temperature).toEqual(20);
+    });
+
+    describe('Energy usage', function () {
+    afterEach(function () {
       thermostat.reset();
+    });
+    it('should return "low-usage" if the temperature is below 18 degrees', function () {
+      thermostat.temperatureDown(3);
+      expect(thermostat.energyUse()).toEqual('low-usage');
+    });
+    it('should return "medium-usage" if the temperature is between 18 and 25 degrees', function () {
+      expect(thermostat.energyUse()).toEqual('medium-usage');
+    });
+    it('should return "high-usage" if the temperature is over 25 degrees', function () {
+      thermostat.powerSaverOff();
+      thermostat.temperatureUp(10);
+      expect(thermostat.energyUse()).toEqual('high-usage');
     });
   });
 });
