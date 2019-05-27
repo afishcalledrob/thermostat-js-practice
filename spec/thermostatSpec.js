1f8274331924cd74530ff18131dde07172f3d8db
Robert Fishwick <fishwickrob@live.com>
2019-05-27 01:20:59 +0100

    First commit, added thermostat and temperature in/decrease

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,29 @@
+describe('Thermostat', function () {
+  var Thermostat  = require('../lib/thermostat');
+  var thermostat = new Thermostat();
+
+  describe('Temperature Property of Thermostat', function () {
+    it('should have a default temperature of 20 degrees', function () {
+      expect(thermostat.temperature).toEqual(20);
+    });
+  });
+
+  describe('Changing temperature', function () {
+    afterEach(function () {
+      thermostat.temperature = 20;
+    });
+    it('should increase temperature by a specified amount', function () {
+      thermostat.temperatureUp(5);
+      expect(thermostat.temperature).toEqual(25);
+    });
+    it('should decrease temperature by a specified amount', function () {
+      thermostat.temperatureDown(5);
+      expect(thermostat.temperature).toEqual(15);
+    });
+    it('should prevent temperature from decreasing below 10', function () {
+      expect(function () {
+        thermostat.temperatureDown(11)
+      }).toThrow('Minimum temperature is 10 degrees');
+    });
+  });
+});
